a89f5a0f26eb071eb394b9ca5a9575df354bab66
wealthforgerbusiness-source <wealthforgerbusiness@gmail.com>
2026-08-18 19:20:45 +0100

    Create sources.js

---
+++ backend/news/sources.js
@@ -0,0 +1,23 @@
+/**
+ * Configurations des sources d'actualités externes autorisées
+ */
+module.exports = [
+  {
+    id: "actualite_cd",
+    name: "Actualite.cd",
+    url: "https://actualite.cd/rss.xml",
+    categoryDefault: "Actualités"
+  },
+  {
+    id: "radiookapi",
+    name: "Radio Okapi",
+    url: "https://www.radiookapi.net/rss.xml",
+    categoryDefault: "Société"
+  },
+  {
+    id: "desk_eco",
+    name: "Desk Eco RDC",
+    url: "https://deskecom.cd/rss.xml",
+    categoryDefault: "Économie"
+  }
+];
